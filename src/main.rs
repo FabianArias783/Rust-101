04ebb4d7f5207992ab3f0f7326e6d9f6d89aef82
FabianArias783 <leonardo.arias4235@alumnos.udg.mx>
2025-03-08 09:03:30 -0600

    2nd Commit

--- src/main.rs
+++ src/main.rs
@@ -1,24 +1,23 @@
 fn main() {
-    //let x = 5;
-    //x=10; ← Esto no va a funcionar porque x es inmutable
-
-    //let mut y = 10;
-    //y = 20; // Esto si funciona porque y es mutable
-
-    let x= 5;
-    let x= x+1; //Nueva variable
-    print!("El valor de x es: {}", x);
+    let x = 5;
+    let x = x + 1; // Nueva variable
+    println!("El valor de x es: {}", x);
 
     println!("Tupla forma 1: ");
 
-    //Variables
+    // Variables
     let entero: i32 = 42;
     let flotante: f64 = 3.1416;
     let booleano: bool = true;
     let caracter: char = 'a';
-    //Tupla → Structs || Creación de tupla llamada firulais
+
+    // Tupla → Structs || Creación de tupla llamada firulais
     let _firulais: (i32, f64, char) = (42, 3.1416, 'a');
     let arreglo: [i32; 3] = [1, 2, 3];
-    print!("Tupla(firulais) forma1: {:?}", _firulais);
-    print!("Tupla(firulais) forma2:  {} {} {}", _firulais.0, _firulais.1, _firulais.2);
+
+    // Forma 1 de imprimir la tupla (con `{:?}`)
+    println!("Tupla(firulais) forma1: {:?}", _firulais);
+
+    // Forma 2 de imprimir la tupla (accediendo a cada elemento de la tupla)
+    println!("Tupla(firulais) forma2:  {} {} {}", _firulais.0, _firulais.1, _firulais.2);
 }
